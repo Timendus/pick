f8c59722601e4778416e3e8211a69b4d19b18ac9
Tim Franssen <tim.franssen@nedap.com>
2018-09-20 19:21:07 +0200

    Make a start at a client for fetching and displaying song chords

---
+++ README.md
@@ -0,0 +1,16 @@
+[![Built with pwa–starter–kit](https://img.shields.io/badge/built_with-pwa–starter–kit_-blue.svg)](https://github.com/Polymer/pwa-starter-kit "Built with pwa–starter–kit")
+
+## Running
+
+```bash
+$ npm start
+```
+
+## Deploying
+
+```bash
+$ npm run build:static
+$ npm run publish
+
+# Git add, commit and push
+```
---
+++ pick-magic.js
@@ -0,0 +1,70 @@
+window.addEventListener('load', function() {
+
+  console.log("Initializing");
+
+  window.globalSongList = {};
+
+  window.selectSong = function(id) {
+    function markupChords(lyrics) {
+      return lyrics;
+    }
+
+    var song = window.globalSongList[id];
+    document.getElementById("song-title").innerHTML = song.song_name + " - " + song.artist_name;
+    document.getElementById("song-lyrics").innerHTML = markupChords(song.lyrics);
+    selectPage('song-page');
+  }
+
+  window.selectPage = function(page) {
+    document.querySelectorAll('.page').forEach(function(e) {e.classList.remove('active')});
+    document.getElementById(page).classList.add('active');
+  }
+
+
+
+  function redraw() {
+    console.log("redraw!");
+    console.log(globalSongList);
+
+    document.getElementById("song-list").innerHTML = generateSongList(window.globalSongList);
+  }
+
+  function generateSongList(songList) {
+    var html = "";
+    Object.entries(songList).forEach(function(song) {
+      html += "<li onclick='selectSong("+song[0]+")'>"+song[1].song_name+" - "+song[1].artist_name+"</li>";
+    });
+    return html;
+  }
+
+  function loadList() {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://limitless-bastion-37095.herokuapp.com/api/songs', true);
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        try {
+          var song_list = JSON.parse(request.responseText);
+        } catch(e) {
+          console.log("Error parsing JSON: ", e);
+        }
+
+        // You could reformat the data in the right format as well:
+        const products = song_list.reduce((obj, product) => {
+          obj[product.id] = product
+          return obj
+        }, {});
+
+        window.globalSongList = products;
+        redraw();
+      } else {
+        console.log("Server returned an error:", request);
+      }
+    };
+    request.onerror = function() { console.log("Could not connect", request); };
+    request.send();
+  }
+
+  loadList();
+
+});
