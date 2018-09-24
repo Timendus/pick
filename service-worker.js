a9e82c6817f23bcedd678a8ade46e71a7ffcc7e3
Tim Franssen <tim.franssen@nedap.com>
2018-09-24 17:48:34 +0200

    Clean up a bit, add service worker for caching API results

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,26 @@
-/**
- * @license
- * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
- * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
- * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
- * Code distributed by Google as part of the polymer project is also
- * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
- */
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/index.html',
+          '/pick-styling.css',
+          '/pick-magic.js'
+        ]
+      );
+    })
+  );
+});
 
-console.info('Service worker disabled for development, will be generated at build time.');
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('songs-api').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
