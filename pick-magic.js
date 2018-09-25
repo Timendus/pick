067e69a0641bfd290e9fc8fb458c30bfc7346c28
Tim Franssen <tim.franssen@nedap.com>
2018-09-25 23:21:10 +0200

    Build css from SCSS in assets/css and javascripts from all files in assets/js

--- pick-magic.js
+++ pick-magic.js
@@ -1,8 +1,39 @@
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+  console.log("Initializing Pick"); // Service worker install
 
-  console.log("Initializing Pick");
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  } // PWA install prompt
 
-  // Global song list
+
+  let deferredPrompt;
+  window.addEventListener('beforeinstallprompt', e => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault(); // Stash the event so it can be triggered later.
+
+    deferredPrompt = e; // Show the install button
+
+    document.querySelectorAll('.install-button').forEach(function (b) {
+      b.classList.add('active');
+      b.addEventListener('click', install2HS);
+    });
+  });
+
+  function install2HS() {
+    // Hide install button
+    document.querySelectorAll('.install-button').forEach(function (b) {
+      b.classList.remove('active');
+    });
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      if (choiceResult.outcome !== 'accepted') {
+        document.querySelectorAll('.install-button').forEach(function (b) {
+          b.classList.add('active');
+        });
+      }
+    });
+  } // Global song list
+
 
   function getSongList() {
     return JSON.parse(localStorage.getItem('songList') || "{}");
@@ -15,111 +46,135 @@ window.addEventListener('load', function() {
   function addSongs(list) {
     var newList = Object.assign(getSongList(), list);
     localStorage.setItem('songList', JSON.stringify(newList));
-  }
+  } // Global song history
 
-  // Global song history
 
   function updateSongHistory(song) {
     var songHistory = JSON.parse(localStorage.getItem('songHistory') || "[]");
-    if ( songHistory.includes(song.id) ) { return; }
-    songHistory.push(song.id);
-    songHistory = songHistory.slice(0,10);
-    localStorage.setItem('songHistory', JSON.stringify(songHistory));
-  }
 
-  // Navigation
+    if (songHistory.includes(song.id)) {
+      return;
+    }
+
+    songHistory.push(song.id);
+    songHistory = songHistory.slice(0, 10);
+    localStorage.setItem('songHistory', JSON.stringify(songHistory));
+  } // Navigation
+
 
   function selectPage(page) {
-    document.querySelectorAll('.page').forEach(function(e) {e.classList.remove('active')});
+    document.querySelectorAll('.page').forEach(function (e) {
+      e.classList.remove('active');
+    });
     document.getElementById(page).classList.add('active');
-    window.scrollTo(0,0);
-  }
+    window.scrollTo(0, 0);
+  } // Searching
 
-  // Searching
 
   function searchSong(e) {
-      e.preventDefault();
-      query = document.getElementById("search-song").value;
-      if ( query == "" ) { renderHistory(); return false; }
-      var request = new XMLHttpRequest();
-      request.open('GET', 'https://limitless-bastion-37095.herokuapp.com/api/songs?query='+encodeURIComponent(query), true);
-      request.onload = function() {
-        if (request.status >= 200 && request.status < 400) {
-          // Success!
-          try {
-            var song_list = JSON.parse(request.responseText);
-          } catch(e) {
-            console.log("Error parsing JSON: ", e);
-          }
+    e.preventDefault();
+    query = document.getElementById("search-song").value;
 
-          // You could reformat the data in the right format as well:
-          const products = song_list.reduce((obj, product) => {
-            obj[product.id] = product
-            return obj
-          }, {});
-
-          addSongs(products);
-          render(products);
-        } else {
-          console.log("Server returned an error:", request);
-        }
-      };
-      request.onerror = function() { console.log("Could not connect", request); };
-      request.send();
+    if (query == "") {
+      renderHistory();
       return false;
-  }
+    }
 
-  // Displaying lists of sings
-  function searchChords(e){
-    var chordsChecked = Array.from(document.getElementsByName('chords')).filter((checkbox) => checkbox.checked).map((checkbox) => `chord=${encodeURIComponent(checkbox.id)}`);
-    var query = chordsChecked.join('&');
-    if ( query == "" ) { renderHistory(); return false; }
     var request = new XMLHttpRequest();
-    request.open('GET', 'https://limitless-bastion-37095.herokuapp.com/api/songs?'+ query, true);
-    request.onload = function() {
+    request.open('GET', 'https://limitless-bastion-37095.herokuapp.com/api/songs?query=' + encodeURIComponent(query), true);
+
+    request.onload = function () {
       if (request.status >= 200 && request.status < 400) {
         // Success!
         try {
           var song_list = JSON.parse(request.responseText);
-        } catch(e) {
+        } catch (e) {
           console.log("Error parsing JSON: ", e);
-        }
+        } // You could reformat the data in the right format as well:
 
-        // You could reformat the data in the right format as well:
+
         const products = song_list.reduce((obj, product) => {
-          obj[product.id] = product
-          return obj
+          obj[product.id] = product;
+          return obj;
         }, {});
-
         addSongs(products);
         render(products);
       } else {
         console.log("Server returned an error:", request);
       }
     };
-    request.onerror = function() { console.log("Could not connect", request); };
+
+    request.onerror = function () {
+      console.log("Could not connect", request);
+    };
+
+    request.send();
+    return false;
+  } // Displaying lists of sings
+
+
+  function searchChords(e) {
+    var chordsChecked = Array.from(document.getElementsByName('chords')).filter(checkbox => checkbox.checked).map(checkbox => `chord=${encodeURIComponent(checkbox.id)}`);
+    var query = chordsChecked.join('&');
+
+    if (query == "") {
+      renderHistory();
+      return false;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'https://limitless-bastion-37095.herokuapp.com/api/songs?' + query, true);
+
+    request.onload = function () {
+      if (request.status >= 200 && request.status < 400) {
+        // Success!
+        try {
+          var song_list = JSON.parse(request.responseText);
+        } catch (e) {
+          console.log("Error parsing JSON: ", e);
+        } // You could reformat the data in the right format as well:
+
+
+        const products = song_list.reduce((obj, product) => {
+          obj[product.id] = product;
+          return obj;
+        }, {});
+        addSongs(products);
+        render(products);
+      } else {
+        console.log("Server returned an error:", request);
+      }
+    };
+
+    request.onerror = function () {
+      console.log("Could not connect", request);
+    };
+
     request.send();
     return false;
   }
 
   function renderHistory() {
-      var songHistory = JSON.parse(localStorage.getItem('songHistory') || "[]");
-      var songHistoryList = songHistory.map(function(id) { return getSong(id); });
-      render(songHistoryList.reverse(), false);
+    var songHistory = JSON.parse(localStorage.getItem('songHistory') || "[]");
+    var songHistoryList = songHistory.map(function (id) {
+      return getSong(id);
+    });
+    render(songHistoryList.reverse(), false);
   }
 
-  function render(songList, searchResults=true) {
+  function render(songList, searchResults = true) {
     // Refresh list
     document.getElementById("song-list").innerHTML = generateSongList(songList);
-    if ( searchResults ) {
+
+    if (searchResults) {
       document.querySelector("#list-page h1").innerText = "Search results";
     } else {
       document.querySelector("#list-page h1").innerText = "Recent songs";
-    }
+    } // Update event handlers
 
-    // Update event handlers
-    document.querySelectorAll('.song-link').forEach(function(b) {
-      b.addEventListener('click', function(e) {
+
+    document.querySelectorAll('.song-link').forEach(function (b) {
+      b.addEventListener('click', function (e) {
         selectSong(e.target.getAttribute('song-id'));
       });
     });
@@ -127,13 +182,12 @@ window.addEventListener('load', function() {
 
   function generateSongList(songList) {
     var html = "";
-    Object.entries(songList).forEach(function(song) {
-      html += "<li><a class='song-link' song-id='"+song[1].id+"'>"+song[1].song_name+" - "+song[1].artist_name+"</a></li>";
+    Object.entries(songList).forEach(function (song) {
+      html += "<li><a class='song-link' song-id='" + song[1].id + "'>" + song[1].song_name + " - " + song[1].artist_name + "</a></li>";
     });
     return html;
-  }
+  } // Displaying individual songs
 
-  // Displaying individual songs
 
   function markupChords(lyrics) {
     return lyrics.replace(/\[ch\]([\w\#\/\\]*)\[\/ch\]/gi, "<span class='chord'>$1</span>");
@@ -145,27 +199,19 @@ window.addEventListener('load', function() {
     document.getElementById("song-lyrics").innerHTML = markupChords(song.lyrics);
     selectPage('song-page');
     updateSongHistory(song);
-  }
+  } // Attach events to buttons
 
 
-
-  // Attach events to buttons
-  document.querySelectorAll('#search-control').forEach(function(b) {
+  document.querySelectorAll('#search-control').forEach(function (b) {
     b.addEventListener('submit', searchSong);
   });
-  document.querySelectorAll('.back-button').forEach(function(b) {
-    b.addEventListener('click', function() {
+  document.querySelectorAll('.back-button').forEach(function (b) {
+    b.addEventListener('click', function () {
       selectPage('list-page');
     });
   });
   document.getElementsByName('chords').forEach(el => el.addEventListener('click', searchChords));
-
   renderHistory();
 });
 
-// Load and register pre-caching Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js');
-  });
-}
+//# sourceMappingURL=pick-magic.js.map
